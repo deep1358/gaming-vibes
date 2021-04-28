b4f368c31958859100c3c6aebc04359613cc4fbe
Deep Shah <deepshah1358@gmail.com>
2021-04-28 16:19:28 +0530

    auhtready

--- stores/authContext.jsx
+++ stores/authContext.jsx
@@ -10,6 +10,7 @@ const AuthContext = createContext({
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [authReady, setAuthReady] = useState(false);
 
   useEffect(() => {
     netlifyIdentity.on("login", (user) => {
@@ -18,6 +19,12 @@ export const AuthContextProvider = ({ children }) => {
     });
     netlifyIdentity.on("logout", () => {
       setUser(null);
+    });
+
+    netlifyIdentity.on("init", (user) => {
+      setUser(user);
+      setAuthReady(true);
+      console.log(user);
     });
 
     netlifyIdentity.init();
@@ -40,6 +47,7 @@ export const AuthContextProvider = ({ children }) => {
     user,
     login,
     logout,
+    authReady,
   };
 
   return (
